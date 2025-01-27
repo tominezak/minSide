e2027efe46e1db06582b67cf4da7bc61159bce50
tominezak <143406731+tominezak@users.noreply.github.com>
2025-01-27 13:21:06 +0100

    Lagt til footer

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,42 +1,62 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import TZlogo from "../images/TZlogo.jpg";
+import { NavLink } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500">
-      {/* Background gradient for the full page */}
-      <nav className="bg-white bg-opacity-20 backdrop-blur-md text-white p-4 fixed top-0 left-0 w-full z-10">
-        {/* Transparent navbar with a slight blur */}
-        <div className="flex justify-between items-center">
-          {/* Logo */}
-          <a href="/">
-            <img src={TZlogo} alt="TZ Logo" className="h-12 w-auto" />
-          </a>
+    <nav className="bg-white bg-opacity-20 backdrop-blur-md text-white p-4 fixed top-0 left-0 w-full z-10">
+      {/* Transparent navbar with a slight blur */}
+      <div className="flex justify-between items-center">
+        {/* Logo */}
+        <img
+          src={TZlogo}
+          alt="TZ Logo"
+          className="h-12 w-auto hover:scale-125 transition-transform duration-300 cursor-pointer"
+        />
 
-          {/* Navigation Links */}
-          <section>
-            <ul className="flex gap-6">
-              <li>
-                <Link to="/" className="hover:underline">
-                  Om meg
-                </Link>
-              </li>
-              <li>
-                <Link to="/projects" className="hover:underline">
-                  Prosjekter
-                </Link>
-              </li>
-              <li>
-                <Link to="/cv" className="hover:underline">
-                  CV
-                </Link>
-              </li>
-            </ul>
-          </section>
-        </div>
-      </nav>
-    </div>
+        {/* Navigation Links */}
+        <section>
+          <ul className="flex gap-6">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `px-3 py-2 rounded-lg transition-all duration-300 cursor-pointer ${
+                    isActive ? "bg-black" : "hover:bg-black"
+                  }`
+                }
+              >
+                Om meg
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/projects"
+                className={({ isActive }) =>
+                  `px-3 py-2 rounded-lg transition-all duration-300 cursor-pointer ${
+                    isActive ? "bg-black" : "hover:bg-black"
+                  }`
+                }
+              >
+                Prosjekter
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/cv"
+                className={({ isActive }) =>
+                  `px-3 py-2 rounded-lg transition-all duration-300 cursor-pointer ${
+                    isActive ? "bg-black" : "hover:bg-black"
+                  }`
+                }
+              >
+                CV
+              </NavLink>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </nav>
   );
 };
 
